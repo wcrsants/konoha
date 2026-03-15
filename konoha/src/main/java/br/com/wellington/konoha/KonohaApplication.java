888883ee288ececd49d8e65bec4411abf1c66197
Wellington Cezar Reis <wcrsants@gmail.com>
2026-03-14 23:29:12 -0300

    first commit

---
+++ konoha/src/main/java/br/com/wellington/konoha/KonohaApplication.java
@@ -0,0 +1,13 @@
+package br.com.wellington.konoha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonohaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonohaApplication.class, args);
+	}
+
+}
